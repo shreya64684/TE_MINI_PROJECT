b89fc1d3b880384ff46f9f170e8b622177618843
Adarsh <addusingh173@gmail.com>
2024-10-21 11:49:25 +0530

    scope Emissions

--- src/pages/Standards.jsx
+++ src/pages/Standards.jsx
@@ -14,11 +14,20 @@ const Standards = () => {
         </figcaption>
       </figure>
 
+
+      <div className="container my-4 py-4 px-8 bg-slate-100">
+
+        <p class="mb-3 text-lg text-gray-800 md:text-xl dark:text-gray-600">GHG Protocol supplies the world's most widely used greenhouse gas accounting standards and guidance.</p>
+        <p class="text-gray-500 dark:text-gray-400">The standards and guidance below are designed to provide a framework for businesses, governments, and other entities to measure and report their greenhouse gas emissions in ways that support their missions and goals.</p>
+
+      </div>
+
+
       <div className="container mx-auto p-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          <Card title="Scope 1 Emission" description="Desc1" />
-          <Card title="Scope 2 Emission" description="Desc2" />
-          <Card title="Scope 3 Emission" description="Desc3" />
+          <Card title="Scope 1 Emission" description="Scope 1 covers emissions from sources that an organisation owns or controls directly – for example from burning fuel in our fleet of vehicles (if they’re not electrically-powered)." />
+          <Card title="Scope 2 Emission" description="Scope 2 are emissions that a company causes indirectly and come from where the energy it purchases and uses is produced. For example, the emissions caused when generating the electricity that we use in our buildings would fall into this category." />
+          <Card title="Scope 3 Emission" description="Scope 3 encompasses emissions that are not produced by the company itself and are not the result of activities from assets owned or controlled by them, but by those that it’s indirectly responsible for up and down its value chain. An example of this is when we buy, use and dispose of products from suppliers. Scope 3 emissions include all sources not within the scope 1 and 2 boundaries." />
         </div>
       </div>
     </div>
